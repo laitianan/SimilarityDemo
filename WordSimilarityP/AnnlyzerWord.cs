ba7088a158a2d59cbcd7660ae3b14a4f388c8057
laitianan <986088865@qq.com>
2017-11-03 13:56:45 +0800

    匹配

---
+++ WordSimilarityP/AnnlyzerWord.cs
@@ -0,0 +1,172 @@
+﻿using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Cn.PowerAnalyzers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Lucene.Net.Analysis.Tokenattributes;
+using System.Text.RegularExpressions;
+namespace WordSimilarityP
+{
+    /// <summary>
+    /// 分词并处理相似度
+    /// </summary>
+    public class AnnlyzerWord
+    {
+        /// <summary>
+        ///字符串分词之后返回一个不重复的数组
+        /// </summary>
+        /// <param name="text">待分词的字符串</param>
+        /// <returns></returns>   wordSegmentation
+        public static SortedSet<string> wordSegmentation(string text)
+        {
+            SortedSet<string> set = new SortedSet<string>();
+            using (TextReader reader = new StringReader(text))
+            {
+                Analyzer analyzer = BeyebeAnalyzer.ReadAnalyzer;
+                TokenStream stream = analyzer.ReusableTokenStream(string.Empty, reader);
+                //Lucene.Net.Analysis.Token token = null;
+                while (stream.IncrementToken())
+                {
+                    ITermAttribute termAttribute = stream.GetAttribute<ITermAttribute>();
+                    set.Add(termAttribute.Term);
+                }
+            }
+            return set;
+        }
+
+
+        /// <summary>
+        ///字符串分词之后返回一个重复的数组
+        /// </summary>
+        /// <param name="text">待分词的字符串</param>
+        /// <returns></returns>   wordSegmentation
+        public static List<string> wordSegmentationToList(string text)
+        {
+            
+            List<string> list = new List<string>();
+            using (TextReader reader = new StringReader(text))
+            {
+                Analyzer analyzer = BeyebeAnalyzer.ReadAnalyzer;
+                TokenStream stream = analyzer.ReusableTokenStream(string.Empty, reader);
+                //Lucene.Net.Analysis.Token token = null;
+                while (stream.IncrementToken())
+                {
+                    ITermAttribute termAttribute = stream.GetAttribute<ITermAttribute>();
+                    list.Add(termAttribute.Term);
+                }
+            }
+            return list;
+        }
+
+        private static Dictionary<string, int> getDictStrNum(List<string> list1)
+        {
+
+            Dictionary<string, int> dict1 = new Dictionary<string, int>();
+            list1.ForEach(r =>
+            {
+                if (dict1.ContainsKey(r)) dict1[r] = dict1[r] + 1;
+                else
+                {
+                    dict1.Add(r, 1);
+                }
+            });
+            return dict1;
+        }
+
+        /// <summary>
+        /// 余弦相似度，值越高相似度越高
+        /// </summary>
+        /// <param name="dict1">字符串向量1</param>
+        /// <param name="dict2">字符串向量2</param>
+        /// <returns></returns>
+        public static double repetitiveRate(Dictionary<string, int> dict1, Dictionary<string, int> dict2)
+        {
+            double f1 = dict1.Values.Sum(r => r * r);
+            double f2 = dict2.Values.Sum(r => r * r);
+            f1 = Math.Sqrt(f1);
+            f2 = Math.Sqrt(f2);
+            int elem = dict1.Aggregate(0, (sum, kv) => sum + kv.Value * (dict2.ContainsKey(kv.Key) ? dict2[kv.Key] : 0));
+            return elem / (f1 * f2);
+        }
+
+        /// <summary>
+        /// 计算两个列别中词的重复率
+        /// </summary>
+        /// <param name="set1">列表一</param>
+        /// <param name="set2">列表二</param>
+        /// <returns>重复率</returns>
+        public static double repetitiveRate(SortedSet<string> set1, SortedSet<string> set2)
+        {
+            double v = set1.Intersect(set2).Count();
+            double u = set1.Union(set2).Count();
+            double p = v / u;
+            return p;
+        }
+        /// <summary>
+        /// 返回列表中商品与productName比较相似度最高的字符串的在list中的索引
+        /// </summary>
+        /// <param name="productName">目标匹配商品</param>
+        /// <param name="productSpec">目标匹配商品规格</param>
+        /// <param name="list">待比较商品名称列表</param>
+        /// <returns>列表中相似度最高的索引位置</returns>
+        public static int wordSimilarityIndex(string productName, string productSpec, List<string> list)
+        {
+            if (list == null || list.Count == 0) throw new Exception("列表中的数据为空");
+            if (list.Count == 1) return 0;
+            productName = productName.ToLower();
+            string productNameDealAfter = Regex.Replace(productName.Replace(productSpec, ""), @"[\(（【][\s\S]*?[\)）】]", "");
+            List<string> setDes = AnnlyzerWord.wordSegmentationToList(productNameDealAfter);
+            var dictDes = AnnlyzerWord.getDictStrNum(setDes);
+            //商品名称   规格  索引
+            Dictionary<string, Tuple<string, int>> dict = new Dictionary<string, Tuple<string, int>>();
+            int i = 0;
+            //获取商品名称上的规格 并转换l to ml，已经存放在list位置上的索引
+            list.ForEach(
+                r =>
+                dict.Add(r.ToLower(), Tuple.Create(UnitConversion.translationLtoML(PatterSpec.patterSpec(r)), i++))
+            );  //使用数据字典存在原来词汇的索引位置
+
+            //商品名称对应的 文本的相似度 以及规格是否相同,初始化索引位置
+            Dictionary<string, Tuple<double, bool, int>> dictMap = new Dictionary<string, Tuple<double, bool, int>>();
+
+            foreach (var item in dict)
+            {
+                //获取相应的分词,分词前先去掉规格;
+                var itemList = AnnlyzerWord.wordSegmentationToList(Regex.Replace(item.Key.Replace(item.Value.Item1, ""), @"[\[(（【](.)*?[)）】\]]", ""));
+                var dictDes2 = AnnlyzerWord.getDictStrNum(itemList);
+                //计算与目标字符串的文本相似度
+                double p = AnnlyzerWord.repetitiveRate(dictDes, dictDes2);
+                bool specEqual = false;
+                if (productSpec != null && item.Value.Item1 != "" && item.Value.Item1 != null)
+                {
+                    specEqual = productSpec.ToLower().Equals(item.Value.Item1);
+                }
+                dictMap.Add(item.Key, Tuple.Create(p, specEqual, item.Value.Item2));
+            }
+
+            var res = dictMap.Values.Where(r => r.Item2);
+            IEnumerable<Tuple<double, bool, int>> orderResult = null;
+            if (res != null && res.Count() > 0)
+            {
+
+                orderResult = res.OrderByDescending(r => r.Item1).Take(2).ToList();
+            }
+            else
+            {
+                orderResult = dictMap.Values.OrderByDescending(r => r.Item1).Take(2).ToList();
+            }
+            //先查找到规格相同的，规格相同的优先级别比较高，在从规格相同的按照相似度的降序排序，
+            //获取出两条数据，再比较两条数据，如果相同相似度则比较初始化位置靠前的。
+            if (orderResult.Count() == 1) return orderResult.First().Item3;
+            else
+            {
+                return orderResult.First().Item1 > orderResult.Last().Item1 ? orderResult.First().Item3 : orderResult.Last().Item3;
+            }
+
+
+        }
+    }
+}
---
+++ WordSimilarityP/PatterSpec.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace WordSimilarityP
+{
+    public class PatterSpec
+    {
+
+
+
+
+        private static string constReigex = "(?i)(ml|L|KG|G|毫升|升|克|千克|片|抽|瓶|罐|粒|盒|袋|块|mm)";
+        public static string intRegex = "((\\d+\\.)?\\d+)";
+        public static string xRegex = "(?i)(\\*|x|×|\\+)";
+        public static string appendUnit = string.Format("((/({0}))?)", constReigex);
+        public static string case1 = intRegex + constReigex + xRegex + intRegex + constReigex + "?";//200*3
+        public static string case2 = intRegex + xRegex + intRegex + constReigex + appendUnit;//200*3L
+        public static string case3 = intRegex + "(" + xRegex + intRegex + ")+";//200*3
+        public static string case4 = intRegex + constReigex + appendUnit;//200L
+        public static string regexString = string.Format("({0}|{1}|{2}|{3})", case1, case2, case3, case4);
+        public static Regex pattern =new Regex(regexString);
+        public static string getSingleSpec(string name)
+        {
+            string result = "";
+            var  matcher = pattern.Match(name);
+            if (matcher.Success)
+            {
+                result = matcher.Value;
+            }
+            return result;
+        }
+
+
+
+        public static String patterSpec(string str_)
+        {
+            str_ = str_.ToLower();
+            string str = patternBracket(str_);
+            //含有括号
+            if (str.Equals(""))
+            {
+                //规格在名称之后
+                str = patternNamePrec(str_);
+            }
+
+            if (str.Equals(""))
+            {
+                //规格在名称之前
+                str = patternPrecName(str_);
+            }
+            if (str.Equals(""))
+                str = getSingleSpec(str_);
+
+            return str;
+        }
+
+     
+        //private static string string_ = "高姿COGI匀净萃白10件套（修容霜5g*2+洁面15g+水10ml*2+乳10g+霜5g+面贴膜20ml*3）（小样套装）不建议购买";
+        // "高姿COGI匀净萃白10件套（修容霜5g*2+洁面15g+水10ml*2+乳10g+霜5g+面贴膜20ml*3）（小样套装）不建议购买";
+        private static Regex pBracket = new Regex("(\\(|（)[^)）]+\\+[^)）]+(\\)|）)");
+
+
+        //用于检测是否规格在之前
+        private static string str2 = string.Format("^\\d+({0})+", constReigex);
+        private static Regex patte = new Regex(str2);
+
+        /**
+         * 规格在前则返回ture
+         *
+         * @param str
+         * @return
+         */
+        private static bool isBefore(string str)
+        {
+            string string_ = str.Trim();
+
+            Match matches = patte.Match(string_);
+            if (matches.Success) return true;
+            return false;
+        }
+
+        //匹配含有括号 "高姿COGI匀净萃白10件套（修容霜5g*2+洁面15g+水10ml*2+乳10g+霜5g+面贴膜20ml*3）（小样套装）不建议购买";
+        private static String patternBracket(string string_)
+        {
+
+            Match m = pBracket.Match(string_);
+            if (m.Success)
+            {
+
+                string str = m.Value;
+                str = str.Replace("(", "").Replace("（", "").Replace(")", "").Replace("）", "");
+                if (isBefore(str))
+                {
+                    return patternPrecName(str);
+                }
+                else
+                {
+                    return patternNamePrec(str);
+                }
+            }
+            return "";
+        }
+
+
+      
+        // 仅仅获取单位
+        private static string unit = string.Format("(\\d+({0}){1}+([*xX]\\d+{2}?)?)([+*]?(\\d+({3}){4}+([*xX]\\d+{5}?)?))*", constReigex, appendUnit, constReigex, constReigex, appendUnit, constReigex);
+        private static Regex unitPattern = new Regex(unit);
+
+
+        //获取规格
+        private static String getUnit(String string_)
+        {
+
+            Match m = unitPattern.Match(string_);
+            //Matcher m = unitPattern.matcher(string_);
+            if (m.Success)
+                return m.Value;
+            return "";
+        }
+
+        // 多芬 滋养水润洗发乳/水/露700ml+护发素195ml*2
+        private static Regex pNamePrec = new Regex(string.Format("([/a-zA-Z\u4e00-\u9fa5])+([ ]+)?(\\d+({0}){1}+([*xX]\\d+)?)([+*]?(\\d+({2}){3}+([*xX]\\d+)?))*",
+            constReigex, appendUnit, constReigex, appendUnit));
+
+        //匹配单位在前面，名称在后面
+        //(\d+%s+(\*\d+)?)([+*]?(\d+%s+(\*\d+)?))?[^ ]+
+        //施华蔻多效修护19套装600ml洗发水600ml护发素送小样
+        private static Regex pPrecName = new Regex(string.Format("(\\d + ({0}){1} + ([*xX]\\d +) ?)([+*] ? (\\d + ({2}){3} + ([*xX]\\d +) ?))?([a-zA-Z\\u4e00-\\u9fa5])+([*]\\d)?",
+            constReigex, appendUnit, constReigex, appendUnit));
+
+        /**
+         * 匹配单位在前面，名称在后面
+         *
+         * @param string
+         * @return
+         */
+        private static String patternPrecName(string string_)
+        {
+
+            MatchCollection matchers = pPrecName.Matches(string_);
+            // Matcher matcher = pPrecName.matcher(string_);
+            StringBuilder builder = new StringBuilder();
+            int k = 0;
+
+            for (int i = 0; i < matchers.Count; i++)
+            {
+                var m = matchers[i];
+                if (m.Success)
+                {
+                    string s = m.Value;
+                    string prec = getUnit(s);
+                    string nameString = s.Replace(prec, "");
+                    builder.Append(nameString + ":" + prec + ":");
+                    k++;
+                }
+            }
+            if (k > 1) return builder.ToString();
+            return "";
+        }
+
+
+        /**
+         * //  // 多芬 滋养水润洗发乳/水/露700ml+护发素195ml*2
+         * 名称在规格之前
+         *
+         * @param string
+         * @return
+         */
+        public static String patternNamePrec(String string_)
+        {
+
+            var ms = pNamePrec.Matches(string_);
+            //Matcher m = pNamePrec.matcher(string_);
+            StringBuilder builder = new StringBuilder();
+            int k = 0;
+            for (int i = 0; i < ms.Count; i++)
+            {
+                var m = ms[i];
+                if (m.Success)
+                {
+                    String s = m.Value;
+                    String prec = getUnit(s);
+                    String nameString = s.Replace(prec, "");
+                    builder.Append(nameString + ":" + prec + ":");
+                    k++;
+                }
+
+            }
+            if (k > 1) return builder.ToString();
+            return "";
+        }
+    }
+}
